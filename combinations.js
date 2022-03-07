296b8fc0579806f00b961160c9a35295295268bd
gary003 <gary.johnson003@gmail.com>
2022-03-07 13:53:59 +0100

    combinations.js

--- combinations.js
+++ combinations.js
@@ -1,15 +1,47 @@
-const combinationsTree = (cards, hand = [], allHands = [], shift = false) => {
-  if (cards.length == 0) return null
+const combinations1Var = (arr) => {
+  let result = [""]
 
-  const [card, ...rest] = cards
+  for (let letter of arr) {
+    result = result.concat(result.map((val) => val.concat(letter)))
+  }
 
-  if (shift) allHands.push(hand)
+  return result
+}
 
-  if (hand.filter((x) => x == card).length < cards.filter((x) => x == card).length) combinationsTree(cards, hand.concat([card]), allHands, true)
+const combinationsTab = (arr) => {
+  const table = Array(arr.length + 1).fill(null)
+  table[0] = [""]
 
-  combinationsTree(rest, hand, allHands, false)
+  for (let i = 0; i < arr.length; i += 1) {
+    table[i + 1] = table[i].map((val) => val + arr[i]).concat(table[i])
+  }
 
-  return allHands
+  return table[arr.length]
+}
+
+const combinationsHO = (arr) => {
+  return [[""]].concat(Array(arr.length - 1).fill(null)).reduce(
+    (acc, _, i) => {
+      return acc.concat([acc[i].map((currAcc) => currAcc + arr[i]).concat(acc[i])])
+    },
+    [[""]]
+  )[arr.length]
+}
+
+const combinationsRedableHO = (arr) => {
+  const table = Array(arr.length).fill(null)
+  table[0] = [""]
+
+  const resultTable = table.reduce(
+    (acc, _, i) => {
+      const newCombi = acc[i].map((currAcc) => currAcc + arr[i])
+      const oldCombi = acc[i]
+      const nextSetOfCombi = newCombi.concat(oldCombi)
+      return acc.concat([nextSetOfCombi])
+    },
+    [[""]]
+  )
+  return resultTable[arr.length]
 }
 
 const combinationsTreeDP = (cards) => {
@@ -26,80 +58,48 @@ const combinationsTreeDP = (cards) => {
   return [...allCombsWithCard, ...allcombsWithoutCard]
 }
 
-const combinationsTab = (arr) => {
-  const table = Array(arr.length + 1).fill(null)
-  table[0] = [""]
+const combinationsTree = (cards, hand = [], allHands = [], shift = false) => {
+  if (cards.length == 0) return null
 
-  for (let i = 0; i < arr.length; i += 1) {
-    table[i + 1] = table[i].map((val) => val + arr[i]).concat(table[i])
-  }
+  const [card, ...rest] = cards
 
-  return table[arr.length]
-}
+  if (shift) allHands.push(hand)
 
-const combinationsTab2 = (arr) => {
-  return [[""]].concat(Array(arr.length - 1).fill(null)).reduce(
-    (acc, val, i) => {
-      return acc.concat([acc[i].map((currAcc) => currAcc + arr[i]).concat(acc[i])])
-    },
-    [[""]]
-  )[arr.length]
-}
+  if (hand.filter((x) => x == card).length < cards.filter((x) => x == card).length) combinationsTree(cards, hand.concat([card]), allHands, true)
 
-const combinationsRedableHO = (arr) => {
-  const table = Array(arr.length).fill(null)
-  table[0] = [""]
+  combinationsTree(rest, hand, allHands, false)
 
-  const resultTable = table.reduce(
-    (acc, val, i) => {
-      const newCombi = acc[i].map((currAcc) => currAcc + arr[i])
-      const oldCombi = acc[i]
-      const nextSetOfCombi = newCombi.concat(oldCombi)
-      return acc.concat([nextSetOfCombi])
-    },
-    [[""]]
-  )
-  return resultTable[arr.length]
-}
-
-const combinationsPush = (arr) => {
-  let result = [""]
-
-  for (let letter of arr) {
-    result = result.concat(result.map((val) => val.concat(letter)))
-  }
-
-  return result
+  return allHands
 }
 
 const testArray = "ABCDEFGHI".split("")
 
-console.time("combinationsTree")
-const comb1 = combinationsTree(testArray)
-// console.log(comb1)
-console.timeEnd("combinationsTree")
-
-console.time("combinationsTreeDP")
-const comb2 = combinationsTreeDP(testArray)
-// console.log((comb2)
-console.timeEnd("combinationsTreeDP")
+console.time("combinations1Var")
+const comb6 = combinations1Var(testArray)
+// console.log(comb6)
+console.timeEnd("combinations1Var")
 
 console.time("combinationsTab")
 const comb3 = combinationsTab(testArray)
 // console.log(comb3)
 console.timeEnd("combinationsTab")
 
-console.time("combinationsTab2")
-const comb4 = combinationsTab2(testArray)
+console.time("combinationsHO")
+const comb4 = combinationsHO(testArray)
 // console.log(comb4)
-console.timeEnd("combinationsTab2")
+console.timeEnd("combinationsHO")
 
 console.time("combinationsRedableHO")
 const comb5 = combinationsRedableHO(testArray)
 // console.log(comb5)
 console.timeEnd("combinationsRedableHO")
 
-console.time("combinationsPush")
-const comb6 = combinationsPush(testArray)
-// console.log(comb6)
-console.timeEnd("combinationsPush")
+console.time("combinationsTreeDP")
+const comb2 = combinationsTreeDP(testArray)
+// console.log((comb2)
+console.timeEnd("combinationsTreeDP")
+
+console.time("combinationsTree")
+const comb1 = combinationsTree(testArray)
+// console.log(comb1)
+console.timeEnd("combinationsTree")
